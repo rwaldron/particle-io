54db01c311bd37d789dd0cabbb4879547f14d534
Chris Williams <chris@iterativedesigns.com>
2014-01-22 20:14:58 -0500

    Now with debug statements dumping to serial port.

--- firmware/firmware.cpp
+++ firmware/firmware.cpp
@@ -1,16 +1,8 @@
-#define DEBUG false;
+int DEBUG=1;
 
 
 TCPClient client;
-long[] SerialSpeed = [600, 1200, 2400, 4800, 9600, 14400, 19200, 28800, 38400, 57600, 115200];
-bool console = false;
-int consoleType = 0;
-
-void log(string) {
-  if (console) {
-    (consoleType ? Serial1.println(string) : Serial.println(string));
-  }
-}
+long SerialSpeed[] = {600, 1200, 2400, 4800, 9600, 14400, 19200, 28800, 38400, 57600, 115200};
 
 void ipArrayFromString(byte ipArray[], String ipString) {
   int dot1 = ipString.indexOf('.');
@@ -27,23 +19,28 @@ int connectToMyServer(String params) {
   int colonIndex = params.indexOf(":");
   String ip = params.substring(0, colonIndex);
   String port = params.substring(colonIndex+1, params.length());
-  (DEBUG && Serial.println("Attempting to connect to server: "+ip+":"+port));
+  if(DEBUG)
+    Serial.println("Attempting to connect to server: "+ip+":"+port);
+
   byte serverAddress[4];
   ipArrayFromString(serverAddress, ip);
   int serverPort = port.toInt();
   if (client.connect(serverAddress, serverPort)) {
-    DEBUG && Serial.println("Connected to server: "+ip+":"+port);
+    if (DEBUG)
+      Serial.println("Connected to server: "+ip+":"+port);
     return 1; // successfully connected
 
   } else {
-    DEBUG && Serial.println("Unable to connect to server: "+ip+":"+port);
+    if(DEBUG)
+      Serial.println("Unable to connect to server: "+ip+":"+port);
     return -1; // failed to connect
   }
 }
 
 void setup() {
   Spark.function("connect", connectToMyServer);
-  DEBUG && Serial.begin(115200);
+  if(DEBUG)
+    Serial.begin(115200);
 
 }
 
@@ -53,11 +50,14 @@ void loop() {
       // parse and execute commands
 
       int action = client.read();
-      DEBUG && Serial.println("Action received: "+('0'+action));
+      if(DEBUG)
+        Serial.println("Action received: "+('0'+action));
+
+      int pin, mode, val, type, speed;
       switch (action) {
         case 0x00:  // pinMode
-          int pin = client.read();
-          int mode = client.read();
+           pin = client.read();
+           mode = client.read();
           if (mode == 0x00) {
             pinMode(pin, INPUT);
           } else if (mode == 0x01) {
@@ -69,51 +69,48 @@ void loop() {
           }
           break;
         case 0x01:  // digitalWrite
-          int pin = client.read();
-          int value = client.read();
-          digitalWrite(pin, value);
+           pin = client.read();
+           val = client.read();
+          digitalWrite(pin, val);
           break;
         case 0x02:  // analogWrite
-          int pin = client.read();
-          int value = client.read();
-          analogWrite(pin, value);
+           pin = client.read();
+           val = client.read();
+          analogWrite(pin, val);
           break;
         case 0x03:  // digitalRead
-          int pin = client.read();
-          int val = digitalRead(pin);
+           pin = client.read();
+           val = digitalRead(pin);
           client.write(0x03);
           client.write(pin);
           client.write(val);
           break;
         case 0x04:  // analogRead
-          int pin = client.read();
-          int val = analogRead(pin);
+           pin = client.read();
+           val = analogRead(pin);
           client.write(0x04);
           client.write(pin);
           client.write(val);
           break;
         case 0x05:  // serial.begin
-          int type = client.read();
-          int speed = client.read();
+           type = client.read();
+           speed = client.read();
           if (type == 0) {
             Serial.begin(SerialSpeed[speed]);
           } else {
             Serial1.begin(SerialSpeed[speed]);
           }
-          console = true;
           break;
         case 0x06:  // serial.end
-          int type = client.read();
+          type = client.read();
           if (type == 0) {
             Serial.end();
           } else {
             Serial1.end();
           }
-          console = false;
           break;
         case 0x07:  // serial.peek
-          int type = client.read();
-          int val;
+          type = client.read();
           if (type == 0) {
             val = Serial.peek();
           } else {
@@ -124,18 +121,6 @@ void loop() {
           client.write(val);
           break;
 
-        case 0x07:  // serial.peek
-          int type = client.read();
-          int val;
-          if (type == 0) {
-            val = Serial.peek();
-          } else {
-            val = Serial1.peek();
-          }
-          client.write(0x07);
-          client.write(type);
-          client.write(val);
-          break;
         default:
           break;
       }
