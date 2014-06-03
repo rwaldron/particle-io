88bb1665a98d01597640d7e3e56c657a06fb9b34
David Resseguie <david@resseguie.com>
2014-06-02 23:25:24 -0400

    Ported some J5 examples to spark-io

---
+++ eg/led-rgb.js
@@ -0,0 +1,60 @@
+var five = require("johnny-five"),
+  Spark = require("../lib/spark"),
+  keypress = require('keypress'),
+  board;
+
+
+// Create Johnny-Five board connected via Spark
+board = new five.Board({
+  io: new Spark({
+    token: process.env.SPARK_TOKEN,
+    deviceId: process.env.SPARK_DEVICE_ID
+  })
+});
+
+// The board's pins will not be accessible until
+// the board has reported that it is ready
+board.on("ready", function() {
+  console.log("CONNECTED");
+  //var a = new five.Led.RGB(["A5","A6","A7"]);
+
+  var a = new five.Led.RGB({
+    pins: {
+      red: "A5",
+      green: "A6",
+      blue: "A7"
+    }
+  });
+
+  a.on();
+  a.color("#FF0000");
+
+  process.stdin.resume();
+  process.stdin.setEncoding('utf8');
+  process.stdin.setRawMode(true);
+
+  process.stdin.on('keypress', function (ch, key) {
+    
+    if ( !key ) {
+      return;
+    }
+
+    if ( key.name == 'r' ) {
+      a.color("#FF0000");
+    }else if ( key.name == 'g' ) {
+      a.color("#00FF00");
+    }else if ( key.name == 'b' ) {
+      a.color("#0000FF");
+    }else if ( key.name == 'w' ) {
+      a.color("#FFFFFF");
+    }else if ( key.name == 'o' ) {
+      a.color("#000000");
+    }
+
+  });
+
+});
+
+board.on("error", function(error) {
+  console.log(error);
+});
---
+++ eg/servo-keypress.js
@@ -0,0 +1,56 @@
+var five     = require("johnny-five");
+var Spark    = require("../lib/spark");
+var keypress = require('keypress');
+
+keypress(process.stdin);
+
+var board = new five.Board({
+  io: new Spark({
+    token: process.env.SPARK_TOKEN,
+    deviceId: process.env.SPARK_DEVICE_ID
+  })
+});
+
+
+board.on("ready", function() {
+
+  console.log("Let's test a simple servo. Use Up and Down arrows for CW and CCW respectively. Space to stop.");
+
+  var servo_pin  = new five.Servo({
+      pin  : "D0",
+      type : 'continuous'
+  }).stop();
+
+  process.stdin.resume();
+  process.stdin.setEncoding('utf8');
+  process.stdin.setRawMode(true);
+
+  process.stdin.on('keypress', function (ch, key) {
+    
+    if ( !key ) return;
+
+    if ( key.name == 'q' ) {
+
+      console.log('Quitting');
+      process.exit();
+
+    } else if ( key.name == 'up' ) {
+
+      console.log('CW');
+      servo_pin.cw();
+
+    } else if ( key.name == 'down' ) {
+
+      console.log('CCW');
+      servo_pin.ccw();
+
+    } else if ( key.name == 'space' ) {
+
+      console.log('Stopping');
+      servo_pin.stop();
+
+    }
+
+  });
+
+});
